5aeae8ad1ff2dfbd9803b94ea98766b940858ad8
Kazooooo <cest93@gmail.com>
2018-04-08 19:06:36 +0900

    ReviewScreenのnavigationOptionsを設定 headerRight押下時にSettingsScreenに遷移させる

--- src/screens/ReviewScreen.tsx
+++ src/screens/ReviewScreen.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { NavigationScreenConfig } from "react-navigation";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends React.Component<{}, {}> {
+  static readonly navigationOptions: NavigationScreenConfig<{}> = ({ navigation }) => ({
+    headerTitle: "Review Jobs",
+    headerRight: <Button title="Settings" onPress={() => navigation.navigate("settings")} />,
+  })
+
   render() {
     return (
       <View>
