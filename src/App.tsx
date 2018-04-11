6c8f3908514008a435f1c003dbe544c9094ee568
Kazooooo <cest93@gmail.com>
2018-04-12 01:34:12 +0900

    TabNavigationを非表示にする

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,10 @@ export default class App extends React.Component {
           },
         }),
       },
+    }, {
+      navigationOptions: {
+        tabBarVisible: false,
+      },
     });
 
     return (
