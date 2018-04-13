fa4fc34b77c6e0dae60419065a2da1219a8b1f8a
Kazooooo <cest93@gmail.com>
2018-04-14 04:23:28 +0900

    ActionをTypeScript FSAに書き換え

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,42 +1,49 @@
-import { AsyncStorage } from "react-native";
-import { createActions } from "redux-actions";
+import actionCreatorFactory from "typescript-fsa";
 import { ThunkAction } from "redux-thunk";
-import { AppState } from "../reducers";
-import { Dispatch } from "redux";
-import { identity } from "lodash";
+import { AsyncStorage } from "react-native";
 import { Facebook } from "expo";
+import { AppState } from "../reducers";
 import { APP_ID } from "../constants/secure/facebookAuth";
+import { AuthState } from "../reducers/authReducer";
 
-const standardActionCreators: {
-  facebook: {
-    loginStart: any;
-    loginSuccess: any;
-    loginFailed: any;
-  };
-} = createActions({
-  FACEBOOK: {
-    LOGIN_START: identity,
-    LOGIN_SUCCESS: identity,
-    LOGIN_FAILED: identity,
-  },
-});
+const actionCreator = actionCreatorFactory("AUTH");
+const loginFacebook = actionCreator.async<{}, AuthState, {}>("LOGIN_FACEBOOK");
 
-const loginFacebookActionCreator = (): ThunkAction<void, AppState, any> => async (
-  dispatch: Dispatch<AppState>,
-) => {
-  dispatch(standardActionCreators.facebook.loginStart());
+const standardActionCreators = {
+  loginStart: loginFacebook.started,
+  loginFailed: loginFacebook.failed,
+  loginSuccess: loginFacebook.done,
+};
+
+const loginFacebookActionCreator = (): ThunkAction<void, AppState, any> => async (dispatch) => {
+  dispatch(standardActionCreators.loginStart({}));
   const token = await AsyncStorage.getItem("fb_token");
   if (token) {
-    dispatch(standardActionCreators.facebook.loginSuccess(token));
+    dispatch(
+      standardActionCreators.loginSuccess({
+        params: {},
+        result: { token },
+      }),
+    );
   } else {
     const response = await Facebook.logInWithReadPermissionsAsync(APP_ID, {
       permissions: ["public_profile"],
     });
     if (response.type === "cancel") {
-      dispatch(standardActionCreators.facebook.loginFailed());
+      dispatch(
+        standardActionCreators.loginFailed({
+          params: {},
+          error: {},
+        }),
+      );
     } else {
       await AsyncStorage.setItem("fb_token", response.token);
-      dispatch(standardActionCreators.facebook.loginSuccess(response.token));
+      dispatch(
+        standardActionCreators.loginSuccess({
+          params: {},
+          result: { token: response.token },
+        }),
+      );
     }
   }
 };
