762bfb2e11b6c0d8f294b2b3d99e4657d6605ed2
Kazooooo <cest93@gmail.com>
2018-04-16 00:40:33 +0900

    MapViewの初期表示となるRegionを設定

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -2,11 +2,31 @@ import React from "react";
 import { View } from "react-native";
 import { MapView } from "expo";
 
-class MapScreen extends React.Component<{}, {}> {
+interface RegionData {
+  longitude: number;
+  latitude: number;
+  longitudeDelta: number;
+  latitudeDelta: number;
+}
+
+interface MapScreenState {
+  region: RegionData;
+}
+
+class MapScreen extends React.Component<{}, MapScreenState> {
+  readonly state: MapScreenState = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09,
+    },
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} />
+        <MapView style={{ flex: 1 }} region={this.state.region} />
       </View>
     );
   }
